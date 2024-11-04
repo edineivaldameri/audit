29c8bdaad0fd3f9eeccc8c570244070f71d6b304
Edinei Valdameri <edineivaldameri@gmail.com>
2024-11-04 17:22:43 -0300

    Remove unnecessary code

--- workbench/app/Http/Controllers/TestController.php
+++ workbench/app/Http/Controllers/TestController.php
@@ -2,17 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Workbench\EdineiValdameri\Laravel\DynamicValidation\App\Http\Controllers;
-
-use EdineiValdameri\Laravel\DynamicValidation\Http\Requests\DynamicFormRequest;
-use Illuminate\Http\JsonResponse;
-
 class TestController
 {
-    public function store(DynamicFormRequest $request): JsonResponse
-    {
-        return response()->json([
-            'message' => 'Validated successfully',
-        ], 200);
-    }
 }
